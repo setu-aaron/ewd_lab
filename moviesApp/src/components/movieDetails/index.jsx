40a1d2b563a56a20b97047b2043c62dc2b0044c1
obriena <aaron@flyingspheres.com>
2023-03-04 14:08:37 -0600

    Show movie review excerpts; Refactor Details page.

--- moviesApp/src/components/movieDetails/index.jsx
+++ moviesApp/src/components/movieDetails/index.jsx
@@ -1,35 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import MonetizationIcon from "@mui/icons-material/MonetizationOn";
 import StarRate from "@mui/icons-material/StarRate";
 import Typography from "@mui/material/Typography";
-import Fab from '@mui/material/Fab';
-import NavigationIcon from '@mui/icons-material/Navigation';
-  
-const styles = { 
-    chipSet: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexWrap: "wrap",
-      listStyle: "none",
-      padding: 1.5,
-      margin: 0,
-    },
-    chipLabel: {
-      margin: 0.5,
-    },
-    fabPosition: {
-      position: "absolute",
-      bottom: 16,
-      right: 16,
-    },
+
+import NavigationIcon from "@mui/icons-material/Navigation";
+import Fab from "@mui/material/Fab";
+import Drawer from "@mui/material/Drawer";
+import MovieReviews from '../movieReviews'
+
+const styles = {
+  chipSet: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap",
+    listStyle: "none",
+    padding: 1.5,
+    margin: 0,
+  },
+  chipLabel: {
+    margin: 0.5,
+  },
+  fab: { 
+    position: "fixed",
+    top: 50,
+    right: 2,
+  },
 };
 
-const MovieDetails = ( props) => {
-  const movie = props.movie
+const MovieDetails = ( {movie}) => {
+  const [drawerOpen, setDrawerOpen] = useState(false); // New
 
   return (
     <>
@@ -63,21 +66,19 @@ const MovieDetails = ( props) => {
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
-      <Paper component="ul" sx={styles.chipSet}>
-          <li>
-            <Chip label="Production Countries" sx={styles.chipLabel} color="primary" />
-          </li>
-          {movie.production_countries.map((g) => (
-            <li key={g.name}>
-              <Chip label={g.name}  />
-            </li>
-          ))}
-      </Paper>
-      <Fab color="error" variant="extended" sx={styles.fabPosition}>
-        <NavigationIcon sx={{ mr: 1 }} />
+      <Fab    
+        color="secondary"
+        variant="extended"
+        onClick={() =>setDrawerOpen(true)}
+        sx={styles.fab}
+      >
+        <NavigationIcon />
         Reviews
       </Fab>
-      </>
+      <Drawer anchor="top" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+        <MovieReviews movie={movie} />
+      </Drawer>
+    </>
   );
 };
 export default  MovieDetails ;
