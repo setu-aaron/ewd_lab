59822256c22a8f92d7950df7f94b0ee967facc23
obriena <aaron@flyingspheres.com>
2023-03-04 14:14:15 -0600

    Full review page

--- moviesApp/src/index.jsx
+++ moviesApp/src/index.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
-
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; 
+import MovieReviewPage from "./pages/movieReviewPage"; 
 const App = () => {
   return (
     <BrowserRouter>
@@ -21,6 +21,7 @@ const App = () => {
         <Route path="/movies/:id/:favorite" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/reviews/:id" element={<MovieReviewPage />} />
       </Routes>
     </BrowserRouter>
   );
