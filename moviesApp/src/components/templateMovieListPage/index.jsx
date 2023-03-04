ec2ba876ccf893a44b5b86cbbe5c5360bdbe5789
obriena <aaron@flyingspheres.com>
2023-03-04 15:01:41 -0600

    Refactor to allow for new filtering hook, and add the hook.

--- moviesApp/src/components/templateMovieListPage/index.jsx
+++ moviesApp/src/components/templateMovieListPage/index.jsx
@@ -1,76 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "../headerMovieList";
-import FilterCard from "../filterMoviesCard";
 import Grid from "@mui/material/Grid";
-import Fab from "@mui/material/Fab";
-import Drawer from "@mui/material/Drawer";
 import MovieList from "../movieList";
 
 const styles = {
-  root: {
-    padding: "20px",
-  },
-  fab: {
-    marginTop: 8,
-    position: "fixed",
-    top: 2,
-    right: 2,
-  },
+  root: { 
+    backgroundColor: "#bfbfbf",
+  }
 };
 
 function MovieListPageTemplate({ movies, title, selectFavourite }) {
-  const [titleFilter, setTitleFilter] = useState("");
-  const [genreFilter, setGenreFilter] = useState("0");
-  const [drawerOpen, setDrawerOpen] = useState(false);
-
-  const genreId = Number(genreFilter);
-
-  let displayedMovies = movies
-    .filter((m) => {
-      return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
-    })
-    .filter((m) => {
-      return genreId > 0 ? m.genre_ids.includes(genreId) : true;
-    });
-
-  const handleChange = (type, value) => {
-    if (type === "title") setTitleFilter(value);
-    else setGenreFilter(value);
-  };
-
   return (
-   <>
-      <Grid container sx={styles.root}>
-        <Grid item xs={12}>
-          <Header title={title} />
-        </Grid>
-        <Grid item container spacing={5}>
-          <MovieList
-            movies={displayedMovies}
-            selectFavourite={selectFavourite}
-          />
-        </Grid>
+    <Grid container sx={styles.root}>
+      <Grid item xs={12}>
+        <Header title={title} />
       </Grid>
-      <Fab
-        color="secondary"
-        variant="extended"
-        onClick={() => setDrawerOpen(true)}
-        sx={styles.fab}
-      >
-        Filter
-      </Fab>
-      <Drawer
-        anchor="left"
-        open={drawerOpen}
-        onClose={() => setDrawerOpen(false)}
-      >
-        <FilterCard
-          onUserInput={handleChange}
-          titleFilter={titleFilter}
-          genreFilter={genreFilter}
-        />
-      </Drawer>
-    </>  
+      <Grid item container spacing={5}>
+        <MovieList
+          selectFavourite={selectFavourite}
+          movies={movies}
+        ></MovieList>
+      </Grid>
+    </Grid>
   );
 }
 export default MovieListPageTemplate;
--- moviesApp/src/pages/upcomingMoviesPage.jsx
+++ moviesApp/src/pages/upcomingMoviesPage.jsx
@@ -1,11 +1,33 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
+import useFiltering from "../hooks/useFiltering";
+import MovieFilterUI, {
+  titleFilter,
+  genreFilter,
+} from "../components/movieFilterUI";
+
+const titleFiltering = {
+  name: "title",
+  value: "",
+  condition: titleFilter,
+};
+const genreFiltering = {
+  name: "genre",
+  value: "0",
+  condition: genreFilter,
+};
+
 
 
 const UpcomingMoviesPage = (props) => {
   const [movies, setMovies] = useState([]);
   const favourites = movies.filter(m => m.favourite)
+  const { filterValues, setFilterValues, filterFunction } = useFiltering(
+    [],
+    [titleFiltering, genreFiltering]
+  );
+
   localStorage.setItem('favourites', JSON.stringify(favourites))
 
   const addToFavourites = (movieId) => {
@@ -15,6 +37,16 @@ const UpcomingMoviesPage = (props) => {
     setMovies(updatedMovies);
   };
 
+    const changeFilterValues = (type, value) => {
+    const changedFilter = { name: type, value: value };
+    const updatedFilterSet =
+      type === "title"
+        ? [changedFilter, filterValues[1]]
+        : [filterValues[0], changedFilter];
+    setFilterValues(updatedFilterSet);
+  };
+
+
   useEffect(() => {
     getUpcomingMovies().then(movies => {
       setMovies(movies);
@@ -22,12 +54,21 @@ const UpcomingMoviesPage = (props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const displayedMovies = filterFunction(movies);
+
   return (
+    <>
     <PageTemplate
       title='Discover Movies'
-      movies={movies}
+      movies={displayedMovies}
       selectFavourite={addToFavourites}
     />
+      <MovieFilterUI
+        onFilterValuesChange={changeFilterValues}
+        titleFilter={filterValues[0].value}
+        genreFilter={filterValues[1].value}
+      />
+      </>
   );
 };
 export default UpcomingMoviesPage;
